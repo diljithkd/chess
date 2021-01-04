6a182f5fdc68a28dd7f2753288809ea938658eec
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2021-01-04 13:28:05 +0530

    Update king.py
    Updated find_next_pos for king

--- king.py
+++ king.py
@@ -1,11 +1,13 @@
 from config import *
 from piece import *
 class King(Piece):
-    def find_next_moves(self):
-        delta = [-1,0,1]
-        poss = []
-        for i in delta:
-            for j in delta:
-                if(not(i==0 and j==0) and self.curr_pos[0] + i >=0 and self.curr_pos[0] + i <8 and self.curr_pos[1] + j >=0 and self.curr_pos[1] + j <8):
-                    poss.append((self.curr_pos[0] +i , self.curr_pos[1]+j ))
-        return poss
+    def find_next_moves(self, board):
+        x_offset = [0,-1,-1,-1,0,1,1,1]
+        y_offset = [-1,-1,0,1,1,1,0,-1]
+        all_pos = []
+        for i,j in zip(x_offset, y_offset):
+            i1 = self.curr_pos[0] + i
+            j1 = self.curr_pos[1] + j
+            if i1>=0 and i1<HEIGHT and j1>=0 and j1<WIDTH and (board[i1][j1]==EMPTY_CELL or (board[i1][j1]!=EMPTY_CELL and board[i1][j1].color!=self.color)):
+                all_pos.append((i1,j1))
+        return all_pos

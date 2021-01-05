f92caf5d53649891a6d42844aad6322aa47ae5bd
DILJITH K D <71268936+diljithkd@users.noreply.github.com>
2021-01-05 18:44:25 +0530

    Update pawn.py
    Starting move 2 cells movement update

--- pawn.py
+++ pawn.py
@@ -14,6 +14,8 @@ class Pawn(Piece):
                     next_pos.append((a-1,b-1))
                 if b < HEIGHT-1 and board[a-1][b+1] != EMPTY_CELL and board[a-1][b+1].color != self.color:
                     next_pos.append((a-1,b+1))
+            if a==6 and board[4][b]==EMPTY_CELL:
+                next_pos.append((4,b))
         else:
             if a==HEIGHT-1:
                 return next_pos
@@ -24,4 +26,6 @@ class Pawn(Piece):
                     next_pos.append((a+1,b-1))
                 if b < HEIGHT-1 and board[a+1][b+1] != EMPTY_CELL and board[a+1][b+1].color != self.color:
                     next_pos.append((a+1,b+1))
+            if a==1and board[3][b]==EMPTY_CELL:
+                next_pos.append((3,b))
         return next_pos
